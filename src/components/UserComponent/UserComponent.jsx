972f8d9097756f6dae36f899c649aedcdb689013
mahiv87 <maherreraiv1987@gmail.com>
2024-03-25 14:24:58 -0600

    active status

--- src/components/UserComponent/UserComponent.jsx
+++ src/components/UserComponent/UserComponent.jsx
@@ -1,12 +1,29 @@
 import styles from '../UserComponent/UserComponent.module.css';
 import avatar from '../../images/image-jeremy.png';
+import { useState } from 'react';
 
 function UserComponent() {
+	const [isActive, setIsActive] = useState(false);
+
 	return (
 		<div className={styles.container}>
-			<img src={avatar} alt="User avatar" className={styles.avatar} />
-			<p>Report for</p>
-			<p>Jeremy Robson</p>
+			<div className={styles.userContainer}>
+				<img src={avatar} alt="User avatar" className={styles.avatar} />
+				<div className={styles.userInfo}>
+					<p>Report for</p>
+					<p className={styles.name}>Jeremy Robson</p>
+				</div>
+			</div>
+			<div className={styles.periodic}>
+				<p
+					className={isActive ? styles.active : ''}
+					onClick={() => setIsActive(!isActive)}
+				>
+					Daily
+				</p>
+				<p>Weekly</p>
+				<p>Monthly</p>
+			</div>
 		</div>
 	);
 }
