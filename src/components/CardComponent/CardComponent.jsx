d4633323e8bf3732095e058191176220215081c2
Marcus mbp <maherreraiv1987@gmail.com>
2024-04-05 22:59:16 -0600

    added switch statement to select correct icon

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from '../CardComponent/CardComponent.module.css';
@@ -10,10 +11,29 @@ import selfIcon from '../../images/icon-self-care.svg';
 import elipsis from '../../images/icon-ellipsis.svg';
 
 function CardComponent({ color, title, time, previous }) {
+	const handleIcon = (name) => {
+		switch (name) {
+			case 'Work':
+				return workIcon;
+			case 'Play':
+				return playIcon;
+			case 'Study':
+				return studyIcon;
+			case 'Exercise':
+				return exerciseIcon;
+			case 'Social':
+				return socialIcon;
+			case 'Self Care':
+				return selfIcon;
+			default:
+				return workIcon;
+		}
+	};
+
 	return (
 		<div style={{ backgroundColor: `${color}` }} className={styles.container}>
 			<div className={styles.imageContainer}>
-				<img src={workIcon} alt="icon" className={styles.icon} />
+				<img src={handleIcon(title)} alt="icon" className={styles.icon} />
 			</div>
 			<div className={styles.activityInfo}>
 				<div className={styles.header}>
@@ -34,8 +54,8 @@ function CardComponent({ color, title, time, previous }) {
 CardComponent.propTypes = {
 	color: PropTypes.string.isRequired,
 	title: PropTypes.string.isRequired,
-	time: PropTypes.string.isRequired,
-	previous: PropTypes.string.isRequired
+	time: PropTypes.number.isRequired,
+	previous: PropTypes.number.isRequired
 };
 
 export default CardComponent;
