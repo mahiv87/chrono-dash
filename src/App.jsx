4af527b995c0c6525ebac8eba79b3374827f0017
Marcus mbp <maherreraiv1987@gmail.com>
2024-04-05 22:23:41 -0600

    dynamically render card data from local data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import UserComponent from './components/UserComponent/UserComponent';
 import CardComponent from './components/CardComponent/CardComponent';
+import data from './db/data.json';
 
 function App() {
 	return (
@@ -8,12 +9,20 @@ function App() {
 			<div className="container">
 				<UserComponent />
 				<div className="cardsContainer">
+					{data &&
+						data.map((activity) => (
+							<CardComponent
+								title={activity.title}
+								time={activity.timeframes.weekly.current}
+								previous={activity.timeframes.weekly.previous}
+							/>
+						))}
+					{/* <CardComponent />
 					<CardComponent />
 					<CardComponent />
 					<CardComponent />
 					<CardComponent />
-					<CardComponent />
-					<CardComponent />
+					<CardComponent /> */}
 				</div>
 			</div>
 		</>
--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -2,7 +2,7 @@ import styles from '../CardComponent/CardComponent.module.css';
 import icon from '../../images/icon-work.svg';
 import elipsis from '../../images/icon-ellipsis.svg';
 
-function CardComponent() {
+function CardComponent({ title, time, previous }) {
 	return (
 		<div className={styles.container}>
 			<div className={styles.imageContainer}>
@@ -10,14 +10,14 @@ function CardComponent() {
 			</div>
 			<div className={styles.activityInfo}>
 				<div className={styles.header}>
-					<p className={styles.title}>Work</p>
+					<p className={styles.title}>{title}</p>
 					<a href="#">
 						<img src={elipsis} alt="elipsis" className={styles.elipsis} />
 					</a>
 				</div>
 				<div className={styles.timeContainer}>
-					<p className={styles.time}>32hrs</p>
-					<p className={styles.previous}>Last Week - 36hrs</p>
+					<p className={styles.time}>{time}hrs</p>
+					<p className={styles.previous}>Last Week - {previous}hrs</p>
 				</div>
 			</div>
 		</div>
