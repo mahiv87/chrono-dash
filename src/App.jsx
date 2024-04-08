d5649fb35814248e75c1be4ac482aa63550211f7
Marcus mbp <maherreraiv1987@gmail.com>
2024-04-08 16:26:09 -0600

    fixed timeframe display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
+// import { useState, useEffect } from 'react';
+// import './App.css';
+// import UserComponent from './components/UserComponent/UserComponent';
+// import CardComponent from './components/CardComponent/CardComponent';
+// import data from './db/data.json';
+
+// function App() {
+// 	const [activeItem, setActiveItem] = useState(1);
+
+// 	useEffect(() => {
+// 		const storedActiveItem = localStorage.getItem('period');
+// 		if (storedActiveItem !== null) {
+// 			setActiveItem(parseInt(storedActiveItem));
+// 		}
+// 	}, []);
+
+// 	console.log(`App: ${activeItem}`);
+
+// 	return (
+// 		<>
+// 			<div className="container">
+// 				<UserComponent activeItem={activeItem} setActiveItem={setActiveItem} />
+// 				<div className="cardsContainer">
+// 					{data.map((activity) => (
+// 						<CardComponent
+// 							key={activity.title}
+// 							activePeriod={Object.keys(activity.timeframes)[activeItem]}
+// 							color={activity.color}
+// 							title={activity.title}
+// 							timeframes={activity.timeframes}
+// 						/>
+// 					))}
+// 				</div>
+// 			</div>
+// 		</>
+// 	);
+// }
+
+// export default App;
 import { useState, useEffect } from 'react';
 import './App.css';
-import UserComponent from './components/UserComponent/UserComponent';
-import CardComponent from './components/CardComponent/CardComponent';
+import avatar from './images/image-jeremy.png';
 import data from './db/data.json';
+import CardComponent from './components/CardComponent/CardComponent';
 
 function App() {
-	const [activeItem, setActiveItem] = useState(1);
+	const [activeItem, setActiveItem] = useState(() => {
+		const storedActiveItem = localStorage.getItem('period');
+		return storedActiveItem !== null ? parseInt(storedActiveItem) : 0;
+	});
+
+	const handleClick = (index) => {
+		setActiveItem(index);
+		localStorage.setItem('period', index.toString());
+	};
 
 	useEffect(() => {
 		const storedActiveItem = localStorage.getItem('period');
@@ -14,25 +61,49 @@ function App() {
 		}
 	}, []);
 
-	console.log(`App: ${activeItem}`);
-
 	return (
-		<>
-			<div className="container">
-				<UserComponent activeItem={activeItem} setActiveItem={setActiveItem} />
-				<div className="cardsContainer">
-					{data.map((activity) => (
-						<CardComponent
-							key={activity.title}
-							activePeriod={Object.keys(activity.timeframes)[activeItem]}
-							color={activity.color}
-							title={activity.title}
-							timeframes={activity.timeframes}
-						/>
-					))}
+		<div className="container">
+			<div className="mainContainer">
+				<div className="userContainer">
+					<img src={avatar} alt="User avatar" className="avatar" />
+					<div className="userInfo">
+						<p>Report for</p>
+						<p className="name">Jeremy Robson</p>
+					</div>
 				</div>
+				<ul className="periodic">
+					<li
+						className={activeItem === 0 ? 'item active' : 'item'}
+						onClick={() => handleClick(0)}
+					>
+						Daily
+					</li>
+					<li
+						className={activeItem === 1 ? 'item active' : 'item'}
+						onClick={() => handleClick(1)}
+					>
+						Weekly
+					</li>
+					<li
+						className={activeItem === 2 ? 'item active' : 'item'}
+						onClick={() => handleClick(2)}
+					>
+						Monthly
+					</li>
+				</ul>
 			</div>
-		</>
+			<div className="cardsContainer">
+				{data.map((activity) => (
+					<CardComponent
+						key={activity.title}
+						activePeriod={Object.keys(activity.timeframes)[activeItem]}
+						color={activity.color}
+						title={activity.title}
+						timeframes={activity.timeframes}
+					/>
+				))}
+			</div>
+		</div>
 	);
 }
 
